ee4306eaf9cbecf06e1897bbb246185fcd22897b
Unknown <destern96@gmail.com>
2017-10-01 02:55:44 -0400

    Added intensity option for spheres

--- First Person Lidar/Assets/CreateSpheres.cs
+++ First Person Lidar/Assets/CreateSpheres.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CreateSpheres : MonoBehaviour {
     public long numDataPoints = 1000; // Pick how many data points to display
     public int j = 0;
+    private static float maxhex = 255;
     public static GameObject[] dataPointArr; // Declare array of data points
         
 	void Start () {
@@ -18,13 +19,16 @@ public class CreateSpheres : MonoBehaviour {
         }
 	}
 
-    public static void update_sphere(long id, float xpos, float ypos, float zpos)
+    public static void update_sphere(long id, float xpos, float ypos, float zpos, float intensity)
     {
         dataPointArr[id].transform.position = new Vector3(xpos, ypos, zpos);
+
+        Color intensityColor = new Color(0, intensity/maxhex, 0, 1);
+        dataPointArr[id].gameObject.GetComponent<Renderer>().material.color = intensityColor;
     }
 
     void Update () {
-        update_sphere(j, j, j, j);
+        update_sphere(j, j, j, j, j);
         j++;
 	}
 }
