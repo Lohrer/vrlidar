cb53473fd2130aa985a98333dd3c00e4028e292e
Michael Lohrer <michael.f.lohrer@gmail.com>
2017-10-01 04:37:56 -0400

    Possible working packet parsing

--- First Person Lidar/Assets/CreateSpheres.cs
+++ First Person Lidar/Assets/CreateSpheres.cs
@@ -89,18 +89,42 @@ public class CreateSpheres : MonoBehaviour {
 
 public class VeloPacket
 {
-    Byte[] raw;
+    public float azimuth;
+    public byte[] intensity;
+    public float[] x;
+    public float[] y;
+    public float[] z;
 
     public VeloPacket(Byte[] data)
     {
-        data.CopyTo(raw, 0);
+        azimuth = 0;
+        intensity = new byte[12 * 32];
+        x = new float[12 * 32];
+        y = new float[12 * 32];
+        z = new float[12 * 32];
         ReadPacket(data);
     }
 
     private void ReadPacket(Byte[] data)
     {
-        // Parse header
-        UInt16 Channel0r = 0;
-        float Channel0r2 = Channel0r / 500f;
+        // Read all 12 azimuths
+        float[] a = new float[24];
+        a[0] = BitConverter.ToUInt16(data, 2) / 100f;
+        azimuth = a[0];
+        for (int i = 1; i < 11; i++)
+        {
+            a[i*2] = BitConverter.ToUInt16(data, i*100+2) / 100f;
+            a[i * 2 - 1] = (a[i * 2 - 2] + a[i * 2]) / 2;
+        }
+        int[] w = {-15, 1, -13, 3, -11, 5, -9, 7, -7, 9, -5, 11, -3, 13, -1, 15};
+        for (int i = 0; i < 12*32; i++)
+        {
+            int index = 100 * (i / 32) + 3 * (i % 32) + 4;
+            float r = BitConverter.ToUInt16(data, index);
+            intensity[i] = data[index + 2];
+            x[i] = r * (float)Math.Cos(w[i % 16]) * (float)Math.Sin(a[i / 16]);
+            y[i] = r * (float)Math.Cos(w[i % 16]) * (float)Math.Cos(a[i / 16]);
+            z[i] = r * (float)Math.Sin(w[i % 16]);
+        }
     }
 }
